c4ed0138bb820dfcd1dc8a16448e9de7336a3eb4
Maximilian Algehed <m.algehed@gmail.com>
2017-08-02 22:51:51 +0200

    Proper unification and proper syntax

--- Sequent.hs
+++ Sequent.hs
@@ -7,6 +7,9 @@ import Data.List hiding (lookup, insert)
 import Data.Generics.Uniplate.Data
 import Data.Map hiding (map)
 
+import Control.Monad
+import Control.Monad.Except
+
 import FOL.Abs
 import FOL.Par
 import FOL.Lex
@@ -15,32 +18,85 @@ import FOL.Print
 
 type Name  = String
 data Param = Param Name [Name]
-  deriving (Ord, Eq, Show, Data, Typeable)
+  deriving (Ord, Eq, Data, Typeable)
 
-data Term = Variable   Name
-          | Parameter  Param
-          | Bound      Int
-          | Function   Name [Term]
-          deriving (Ord, Eq, Show, Data, Typeable)
+instance Show Param where
+  show (Param n ns) = n ++ "<" ++ intercalate ", " (("?"++) <$> ns) ++ ">"
+
+data Term = Variable    Name
+          | SynVariable Name -- Only exists during pretty printing
+          | Parameter   Param
+          | Bound       Int
+          | Function    Name [Term]
+          deriving (Ord, Eq, Data, Typeable)
+
+instance Show Term where
+  show t = case t of
+    Variable n    -> "?" ++ n
+    SynVariable n -> n
+    Parameter p   -> show p
+    Function f ts -> f ++ "(" ++ intercalate ", " (show <$> ts) ++ ")"
 
 data Formula = Predicate      Name  [Term]
              | Connective     Conn  [Formula]
              | Quantification Quant Name Formula
-             deriving (Ord, Eq, Show, Data, Typeable)
+             deriving (Ord, Eq, Data, Typeable)
+
+instance Show Formula where
+  showsPrec p f = case f of
+    Predicate n ts ->
+      showParen (p > 5) $ showString
+        (n ++ "(" ++ intercalate ", " (show <$> ts) ++ ")")
+    Connective Not [f] ->
+      showParen (p > 5) $ showString "~" . showsPrec 5 f
+    Connective c [l, r] ->
+      let prec = precedence c in
+      showParen (p > prec) $
+        showsPrec (prec + 1) l .
+        showString (" " ++ show c ++ " ") .
+        showsPrec prec r
+    Quantification q n f ->
+      showParen (p > 0) $
+        showString (show q ++ " " ++ n ++ ". " ++
+                    show (substitute 0 (SynVariable n) f))
 
 data Conn = And
           | Or
           | Not
           | Impl
           | Eqv
-          deriving (Ord, Eq, Show, Data, Typeable)
+          deriving (Ord, Eq, Data, Typeable)
+
+precedence :: Conn -> Int
+precedence c = case c of
+  And  -> 4
+  Or   -> 3
+  Not  -> 5
+  Impl -> 2
+  Eqv  -> 1
+
+instance Show Conn where
+  show c = case c of
+    And  -> "&"
+    Or   -> "|"
+    Impl -> "->"
+    Eqv  -> "="
+    Not  -> "~"
 
 data Quant = EX
            | ALL
-           deriving (Ord, Eq, Show, Data, Typeable)
+           deriving (Ord, Eq, Data, Typeable)
+
+instance Show Quant where
+  show EX  = "EX"
+  show ALL = "ALL"
 
 data Sequent = [Formula] :|- [Formula]
   deriving (Ord, Eq, Data, Typeable)
+
+instance Show Sequent where
+  show (lhs :|- rhs) =
+    intercalate ", " (show <$> lhs) ++ " |- " ++ intercalate ", " (show <$> rhs)
 
 parseSequent :: String -> Either String Sequent
 parseSequent s = case pSSequent (myLexer s) of 
@@ -49,7 +105,7 @@ parseSequent s = case pSSequent (myLexer s) of
     (translateForm empty <$> lhs) :|- (translateForm empty <$> rhs)
   where
     translateForm m f = case f of
-      FPred (Ident id) ts -> Predicate id    (translateTerm m <$> ts) 
+      FPred (Ident id) ts -> Predicate id (translateTerm m <$> ts) 
       FNot  f   -> Connective Not  [translateForm m f] 
       FAnd  f g -> Connective And  [translateForm m f, translateForm m g]
       FOr   f g -> Connective Or   [translateForm m f, translateForm m g]
@@ -60,43 +116,17 @@ parseSequent s = case pSSequent (myLexer s) of
 
     translateTerm m t = case t of
       TVar (Ident id)
-        | member id m -> let Just i = lookup id m in Bound i
         | otherwise   -> Variable id 
+      TBVar (Ident id)
+        | member id m -> let Just i = lookup id m in Bound i
+        | otherwise   -> Variable id
       TParam p              -> Parameter (translateParam p)
       TFunApp (Ident id) ts -> Function id (translateTerm m <$> ts)
 
-    translateParam (PParam (PIdent (Ident id)) ids) =
+    translateParam (PParam (Ident id) ids) =
       Param id [ id | PIdent (Ident id) <- ids ]
 
     extend id m = insert id 0 ((+1) <$> m)
-
-printSequent :: Sequent -> String
-printSequent = printTree . translateSequent 
-  where
-    translateSequent (lhs :|- rhs) =
-      Seq (translateForm empty <$> lhs) (translateForm empty <$> rhs)
-
-    translateForm m f = case f of
-      Predicate n ts          -> FPred (Ident n) (translateTerm m <$> ts)
-      Connective Not  [t]     -> FNot  (translateForm m t)
-      Connective And  [t, t'] -> FAnd  (translateForm m t) (translateForm m t')
-      Connective Or   [t, t'] -> FOr   (translateForm m t) (translateForm m t')
-      Connective Impl [t, t'] -> FImpl (translateForm m t) (translateForm m t')
-      Connective Eqv  [t, t'] -> FEqv  (translateForm m t) (translateForm m t')
-      Quantification ALL id t -> FALL (Ident id) (translateForm (extend id m) t)
-      Quantification EX  id t -> FEX  (Ident id) (translateForm (extend id m) t)
-
-    translateTerm m t = case t of
-      Variable id            -> TVar (Ident id)
-      Bound i                -> let Just id = lookup i m in TVar (Ident id)
-      Parameter (Param n ns) -> TParam
-        (PParam (PIdent $ Ident n) (PIdent . Ident <$> ns))
-      Function n ts          -> TFunApp (Ident n) (translateTerm m <$> ts)
-    
-    extend id m = insert 0 id (mapKeys (+1) m)
-
-instance Show Sequent where
-  show = printSequent
 
 replace :: Term -> Term -> Term -> Term 
 replace u1 u2 = rewrite go
@@ -120,3 +150,33 @@ variables a = nub [ n | Variable n <- universeBi a ]
 
 parameters :: Data t => t -> [Param]
 parameters = nub . universeBi
+
+type Env = Map Name Term
+
+unify :: Term -> Term -> Either String Env
+unify = go empty
+  where
+    go :: Env -> Term -> Term -> Either String Env
+    go env t u = case (t, u) of
+      (Variable a, _)
+        | u == t                 -> return env
+        | occurs a (apply env u) -> throwError "Occurs check failed"
+        | otherwise              -> return $ insert a u env
+      (_, Variable a)            -> go env u t
+      (Parameter (Param a _), Parameter (Param b _))
+        | a == b    -> return env
+        | otherwise -> throwError "Can not unify different parameters"
+      (Function a ts, Function b us)
+        | a == b    -> foldM (\e (x, y) -> go e x y) env (zip ts us)
+        | otherwise -> throwError "Can not unify different functions"
+      _ -> throwError "Unable to unify terms"
+
+apply :: Data t => Env -> t -> t 
+apply env = rewriteBi go
+  where
+    go (Variable n) = lookup n env
+    go _            = Nothing
+
+occurs :: Name -> Term -> Bool
+occurs n t =
+  elem n (variables t) || elem n (concat [ vs | Param _ vs <- parameters t ])
